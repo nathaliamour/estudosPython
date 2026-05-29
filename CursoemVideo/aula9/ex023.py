d61343ed2daf24a567ec8a4aa0b704523511aa57
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-05-29 19:43:43 -0300

    Atualiza arquivos

--- CursoemVideo/aula9/ex023.py
+++ CursoemVideo/aula9/ex023.py
@@ -2,5 +2,5 @@
 num = input('Digite um número de 0 a 9999: ')
 numConc = '0000' + num
 numI = numConc[::-1]
-print(numI)
+
 print('Unidade: {}.\nDezena: {}.\nCentena: {}.\nMilhar: {}'.format(numI[0], numI[1], numI[2], numI[3]))
