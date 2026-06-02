4d9bb63ca074737760b94e48ca2ca9e390921c0b
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-02 14:06:56 -0300

    Att 024

--- CursoemVideo/aula9/ex024.py
+++ CursoemVideo/aula9/ex024.py
@@ -1,5 +1,5 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 city = (str(input('Digite o nome de uma cidade: '))).upper() 
-print("Verificando se começa com 'SANTO'...'")
+print("Verificando se começa com 'SANTO'..'")
 verifica = city.find('SANTO')
 print(verifica == 0)
