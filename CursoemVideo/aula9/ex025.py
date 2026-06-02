5132623040350bd9ecbe4f19606fb1a35bc90588
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-02 18:33:46 -0300

    Att 025

--- CursoemVideo/aula9/ex025.py
+++ CursoemVideo/aula9/ex025.py
@@ -1,1 +1,4 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
+nome = input('Digite o seu nome completo: ').strip()
+nome = nome.upper()
+print("Seu nome tem 'Silva'? {}".format(nome.find('SILVA')>-1))
