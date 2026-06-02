75515c91b33ead88d61fda6e78b53d524d64a90b
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-02 19:18:34 -0300

    att 027

--- CursoemVideo/aula9/ex027.py
+++ CursoemVideo/aula9/ex027.py
@@ -1,1 +1,9 @@
-#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o 
+#último nome separadamente.
+nome = input("Digite o seu nome completo: ").strip()
+nome = nome.split()
+FirstName = nome[0]
+nome = nome[::-1]
+LastName = nome[0]
+print("O seu primeiro nome é {}, e o seu último sobrenome é {}.".format(FirstName,LastName))
+
