ebb2dfa1f1531e0cc969553c0b400602e9bcc907
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-03 11:25:44 -0300

    att cores

---
+++ CursoemVideo/aula11/aula11.py
@@ -0,0 +1,21 @@
+#ANSI - escape sequence
+#.       \033[0;33;44m
+#STYLE;         TEXT;               BACK
+#                                   mesmas cores, porem
+#0              30 - branco         40
+#1 bold         31 - vermelho       41
+#4 underline    32 - verde          42
+#7 negative     33 - amarelo        43
+#               34 - azul           44
+#               35 - lilas          45
+#               36 - ciano          46
+#               37 - cinza          47
+
+#\033[0;30;41m
+#\033[4;33;44m
+#\033[1;35;43m
+#\033[30;42m
+#\033[m
+#\033[7;30m #inverte cor da letra e fundo
+
+
--- CursoemVideo/aula7/ex001e2.py
+++ CursoemVideo/aula7/ex001e2.py
@@ -1,3 +1,3 @@
-print('Olá, mundo!')
+print('\033[31;43mOlá, mundo!')
 nome = input("Qual o seu nome?")
 print("É um grande prazer te conhecer, ", nome)
