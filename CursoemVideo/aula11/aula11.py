7b93b613970635032adbac935a0aed3bd26f795c
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-03 12:07:24 -0300

    att

--- CursoemVideo/aula11/aula11.py
+++ CursoemVideo/aula11/aula11.py
@@ -11,11 +11,21 @@
 #               36 - ciano          46
 #               37 - cinza          47
 
-#\033[0;30;41m
-#\033[4;33;44m
-#\033[1;35;43m
-#\033[30;42m
-#\033[m
-#\033[7;30m #inverte cor da letra e fundo
+#\033[0;30;41m     \033[m
+#\033[4;33;44m    \033[m
+#\033[1;35;43m    \033[m
+#\033[30;42m    \033[m
+#\033[m    \033[m
+#\033[7;30m     \033[m #inverte cor da letra e fundo
+
+'''
+frase = "A vida é bela"
+cores = {'limpa':'\033[m',
+        'azul':'\033[34m, 
+        'amarelo':'\033[33m', 
+        'pretoebranco':'\033[7;30m]'} 
+
+print("A frase escolhida é {}{}{}!!!".format(cores['pretoebranco'], frase, cores['amarelo'])
+'''
 
 
--- CursoemVideo/aula7/ex001e2.py
+++ CursoemVideo/aula7/ex001e2.py
@@ -1,3 +1,3 @@
-print('\033[31;43mOlá, mundo!')
+print('\033[31;43mOlá, mundo!\033[m')
 nome = input("Qual o seu nome?")
 print("É um grande prazer te conhecer, ", nome)
