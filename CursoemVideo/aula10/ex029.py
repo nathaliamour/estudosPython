9cc032641ec6c193333ec53f4481460bd6d2fbf2
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-03 08:20:11 -0300

    att 029

--- CursoemVideo/aula10/ex029.py
+++ CursoemVideo/aula10/ex029.py
@@ -1,2 +1,9 @@
 #Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80 km/h, 
 #mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada KM acima do limite.
+vel = int(input("Digite o valor da velocidade do veículo, em km/h: "))
+
+if vel > 80: 
+    multa = (vel - 80) * 7
+    print("Você foi multado!\nO valor da multa será de R${}.".format(multa))
+else: 
+    print("Fique tranquilo!\nVocê não foi multado!")
