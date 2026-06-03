8ec80acbc8bcbb516ae377166dc556aa2fafc385
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-03 11:04:00 -0300

    att 032

--- CursoemVideo/aula10/ex032.py
+++ CursoemVideo/aula10/ex032.py
@@ -4,4 +4,4 @@ print("Consultando se {} é bissexto ou não...".format(ano))
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0): 
     print("O ano {} é bissexto!".format(ano))
 else: 
-    print("O ano {} não é bissexto!")
+    print("O ano {} não é bissexto!".format(ano))
