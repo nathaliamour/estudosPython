1cbf1e24fc7d55939653f9e7822b9d00d0199c43
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-03 08:09:54 -0300

    att 028

--- CursoemVideo/aula10/ex028.py
+++ CursoemVideo/aula10/ex028.py
@@ -1,3 +1,13 @@
 #Escreva um programa que faca o computador "pensar" em um numero inteiro entre 0 e 5 e 
 #peca para o usuario tentar descobrir qual foi o numero escolhido pelo computador. O programa devera
 #escrever na tela se o usuario venceu ou perdeu.
+import random 
+
+num = random.randint(1,5)
+
+palpite = int(input("Vou escolher um número entre 1 e 5...\nConsegue adivinhar o número escolhido?\nDigite o seu palpite: "))
+
+if num == palpite:
+    print("Uau! Você venceu!")
+else: 
+    print("Não foi dessa vez!\nrs\nrs\nrs")
