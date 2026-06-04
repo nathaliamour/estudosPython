26bdd6ad1412d33fad8ac66f8d3d84aaa7e76e23
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-04 20:01:41 -0300

    att

--- CursoemVideo/aula12/ex042.py
+++ CursoemVideo/aula12/ex042.py
@@ -4,3 +4,18 @@ Refaça o DESAFIO 035 dos triangulos, acrescentando o recurso de mostrar que tip
 - isósceles: dois lados iguais
 - escaleno: todos os lados diferentes
 """
+print("Vamos trabalhar com triângulos...\nEscolha 3 retas e veremos se elas formam um triângulo!")
+l1 = float(input("Escreva o tamanho da primeira reta: "))
+l2 = float(input("Escreva o tamanho da segunda reta: "))
+l3 = float(input("Escreva o tamanho da terceira reta: "))
+
+if (l1 + l2) > l3 and (l1 + l3) > l2 and (l2 + l3) > l1:
+    print("Uau! As retas {}, {} e {} formam um triângulo!".format(l1, l2, l3))
+    if l1 == l2 and l2 == l3: 
+        print("Se trata de um triângulo equilátero!\n")
+    elif l1 == l2 or l2 == l3: 
+        print("Se trata de um triângulo isósceles!\n")
+    elif l1 != l2 and l2 != l3: 
+        print("Se trata de um triângulo escaleno!\n")
+else: 
+    print("As retas {}, {} e {} não podem formar um triângulo!\n".format(l1, l2, l3))
---
+++ CursoemVideo/aula12/ex044.py
@@ -0,0 +1,8 @@
+"""
+Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e a condição de 
+pagamento: 
+- a vista dinheiro/pix: 10% de desconto
+- a vista no cartao: 5% de desconto
+- em ate 2x no cartao: preço normal
+- 3x ou mais no cartao: 20% de juros 
+""" 
