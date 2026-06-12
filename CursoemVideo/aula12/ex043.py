f70a3f750cc79eb546c042dcfd4adde2934b669a
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-12 14:36:39 -0300

    att

--- CursoemVideo/aula12/ex043.py
+++ CursoemVideo/aula12/ex043.py
@@ -7,3 +7,19 @@ de acordo com a tabela abaixo:
 - 30 a 40: Obesidade
 - Acima de 40: Obesidade Mórbida
 """
+peso = float(input("Digite o seu peso, em kg: "))
+altura = float(input("Digite a sua altura, em metros: "))
+imc = peso / (altura * altura)
+
+print("O seu IMC tem valor {}.".format(imc))
+
+if imc < 18.5:
+    print("Voce está abaixo do peso!")
+elif imc >= 18.5 and imc < 25: 
+    print("Voce está no seu peso ideal!")
+elif imc >= 25 and imc < 30: 
+    print("Voce está com sobrepeso!")
+elif imc >= 30 and imc < 40: 
+    print("Voce está com obesidade!")
+else: 
+    print("Voce está com obesidade mórbida!")
--- CursoemVideo/aula12/ex044.py
+++ CursoemVideo/aula12/ex044.py
@@ -6,3 +6,17 @@ pagamento:
 - em ate 2x no cartao: preço normal
 - 3x ou mais no cartao: 20% de juros 
 """ 
+precoInicial = float(input("Digite o valor inicial do produto: "))
+pagamento = int(input("Digite a forma de pagamento:\n1 - dinheiro/pix\n2 - Cartao a vista\n3 - 2x no cartao\n4 - a partir de 3x no cartao\n"))
+
+if pagamento == 1:
+    newPreco = precoInicial * 0.9
+    print("Preço com desconto - 10%\nPreço a receber: R${}.".format(newPreco))
+elif pagamento == 2:
+    newPreco = precoInicial * 0.95
+    print("Preço com desconto - 5%\nPreço a receber: R${}.".format(newPreco))
+if pagamento == 3:
+    print("Preço sem desconto\nPreço a receber: R${}.".format(precoInicial))
+if pagamento == 4:
+    newPreco = precoInicial * 1.2
+    print("Preço com juros - 20%\nPreço a receber: R${}.".format(newPreco))
