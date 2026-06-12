247480c985abcc1a052be98714c3a9fcf1f08e46
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-12 15:35:03 -0300

    jokenpo

--- CursoemVideo/aula12/ex045.py
+++ CursoemVideo/aula12/ex045.py
@@ -1,3 +1,31 @@
 """
 Crie um programa que faça o computador jogar jokenpô com vc
 """
+import time
+import random
+player = int(input("\nVamos jogar jokenpo!!!\n\nVocê precisa escolher um número, dentre as opções abaixo:\n" \
+"[1] Pedra\n[2] Papel\n[3] Tesoura\n\nQual a sua escolha? "))
+
+jogadas = {
+    1: "Pedra",
+    2: "Papel",
+    3: "Tesoura"
+}
+print("\nJO")
+time.sleep(1)
+print("KEN")
+time.sleep(1)
+print("PO!!!\n")
+time.sleep(1)
+
+pc = random.randint(1, 3)
+
+print("O computador jogou {}".format(jogadas[pc]))
+print("O jogador jogou {}\n".format(jogadas[player]))
+
+if pc == player: 
+    print("Vocês empataram!!! \n Jogue novamente!")
+elif (pc == 1 and player == 2) or (pc == 2 and player == 3) or (pc == 3 and player == 1):
+    print("O JOGADOR venceu!!!")
+else: 
+    print("O COMPUTADOR venceu!!!")
