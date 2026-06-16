cc76254acbfe0a324e8bd7d4693c93ffed21755b
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-15 22:03:39 -0300

    att

--- CursoemVideo/aula13/ex046.py
+++ CursoemVideo/aula13/ex046.py
@@ -3,12 +3,12 @@ Faca um programa que mostre na tela uma contagem regressiva para estouro de fogo
 com uma pausa de um segundo entre eles.
 """
 import time
-print("Contagem regressida para estouro dos fogos!!!")
+print("Contagem regressiva para estouro dos fogos!!!")
 for i in range(10, 0, -1): 
     print(i)
     time.sleep(1)
-print("  \  |  /\n --  *  --\n  /  |  \ ")
+print("\n    * . *\n    *  X  *\n    * . *")
 time.sleep(1)
-print("  * . *\n  *  X  *\n  * . *")
+print("\n   \  |  /\n  --  *  --\n   /  |  \ ")
 time.sleep(1)
-print("     .  :  .\n   :  ' : '  :\n  .  *  X  *  .\n   :  . : .  :\n     '  :  '")
+print("\n     .  :  .\n   :  ' : '  :\n  .  *  X  *  .\n   :  . : .  :\n     '  :  '")
