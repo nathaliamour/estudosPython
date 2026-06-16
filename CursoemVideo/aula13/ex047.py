6abf0aa3e644eba162d2786f560d7d13ac182610
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-15 22:21:07 -0300

    ex047

--- CursoemVideo/aula13/ex047.py
+++ CursoemVideo/aula13/ex047.py
@@ -1,3 +1,5 @@
 """
-Crie um programa que mostre na tela todos os números pares que estao no intevalo entre 1 e 50.
+Crie um programa que mostre na tela todos os números pares que estao no intervalo entre 1 e 50.
 """
+for i in range(0, 50, 2):
+    print(i)
