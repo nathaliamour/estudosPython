9ade4456dbcc59fb3f02faf72cade2cfbdf0fa05
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-16 08:57:15 -0300

    att exerc aula 13

--- CursoemVideo/aula13/ex047.py
+++ CursoemVideo/aula13/ex047.py
@@ -1,5 +1,6 @@
 """
 Crie um programa que mostre na tela todos os números pares que estao no intervalo entre 1 e 50.
 """
+print("Todos os números pares entre 1 e 50 são: ")
 for i in range(0, 51, 2):
     print(i)
--- CursoemVideo/aula13/ex049.py
+++ CursoemVideo/aula13/ex049.py
@@ -1,3 +1,10 @@
 """
 Refaca o desafio 009, mostrando a tabuada de um numero que o usuario escolher, só que agora utilizando um laco for.
-"""
+""" 
+n = int(input('Digite um número inteiro: '))
+
+print('-' * 12)
+print('--Tabuada do {}--'.format(n))
+for i in range(1, 10): 
+    print('{} x {:2} = {}'.format(n, i, n * (i+1)))
+print('-' * 12)
