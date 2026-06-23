24acf4ad14a7c3fd0ba17ecfdc25ee4e8f840c74
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-23 09:37:44 -0300

    aula 14

--- CursoemVideo/aula14/aula14.py
+++ CursoemVideo/aula14/aula14.py
@@ -4,3 +4,18 @@ while c != 10:
     c+=1
 print("Acabou!")
 
+r = 'S'
+while r == 'S':
+    n = int(input("Digite um valor: "))
+    r = str(input("Quer continuar? [S/N] ")).upper()
+print("Fim!")
+
+i = 1
+par = impar = 0
+while n!= 0:
+    i = int(input("Digite um valor: "))
+    if i % 2 == 0: 
+        par += 1
+    else: 
+        impar += 1
+print("Você digitou {} números pares e {} números ímpares!".format(par, impar))
