b5e15aee95e37d8fd671da12e9fe6278e77ab439
Nathália Moura <52115967+nathaliamour@users.noreply.github.com>
2026-06-23 09:52:38 -0300

    att exerc aula14

---
+++ CursoemVideo/aula14/ex061.py
@@ -0,0 +1,4 @@
+"""
+Refaça o desafio 051, lendo o primeiro termo e a razao de uma PA, 
+mostrando os 10 primeiros termos da progressao usando estrutura while.
+"""
---
+++ CursoemVideo/aula14/ex062.py
@@ -0,0 +1,4 @@
+"""
+Melhore o Desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. 
+O programa encerra quando ele disser que quer mostrar 0 termos. 
+"""
