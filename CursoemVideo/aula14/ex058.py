db249195997d8be1ab3cd4668130f5c365f0c66c
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-28 11:54:50 -0300

    ex058 att

--- CursoemVideo/aula14/ex058.py
+++ CursoemVideo/aula14/ex058.py
@@ -1,1 +1,16 @@
 """
+Melhore o jogo do Desafio 028 onde o computador vai "pensar" em um número de 0 a 10. Só que agora o jogador vai tentar adivinhar
+até acertar, mostrando no final quantos palpites foram necessários para vencer.
+"""
+import random 
+num = random.randint(0, 10)
+palpite = int(input("Vou escolher um número entre 0 e 10...\nConsegue adivinhar o número escolhido?\nDigite o seu palpite: "))
+count = 1
+while num != palpite:
+    count += 1 
+    if num == palpite:
+        print("Uau! Você venceu!")
+    else: 
+        print("Não foi dessa vez!\nrs\nrs\nrs\nTente novamente!")
+        palpite = int(input("Digite o seu palpite: "))
+print("Você conseguiu! O número era {} e você precisou de {} palpites para vencer!".format(num, count))
