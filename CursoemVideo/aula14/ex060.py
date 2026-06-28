6f39568115f45ddf0c745d744c87cb787ca338ba
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-28 13:34:34 -0300

    ex60 e 61

--- CursoemVideo/aula14/ex060.py
+++ CursoemVideo/aula14/ex060.py
@@ -1,3 +1,14 @@
 """
 Faça um programa que leia um número qualquer e mostre o seu fatorial.
 """
+print("--Vamos calcular fatorial--")
+num = int(input("Digite o número: "))
+ciclo = num
+fatorial = 1
+print("{}! = ".format(num), end = "")
+while ciclo != 0:
+    fatorial *= ciclo
+    print("{}".format(ciclo), end="")
+    print(" x " if ciclo > 1 else " = ", end = "")
+    ciclo -= 1
+print(fatorial)
--- CursoemVideo/aula14/ex061.py
+++ CursoemVideo/aula14/ex061.py
@@ -2,3 +2,13 @@
 Refaça o desafio 051, lendo o primeiro termo e a razao de uma PA, 
 mostrando os 10 primeiros termos da progressao usando estrutura while.
 """
+print("Brincando com Progressao Aritmética")
+n = int(input("Digite o primeiro termo da P.A.: "))
+r = int(input("Digite a razao dessa P.A.: "))
+c = 10
+print("Primeiro termo: {}\nRazão: {}\nP.A: ".format(n, r), end = "")
+while c != 0: 
+    print("{}".format(n), end = "")
+    print(", " if c > 1 else ".", end = "")
+    n += r
+    c -= 1
