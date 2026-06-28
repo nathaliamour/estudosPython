4a472d1679cc45a9327610d23d50337a991ffda0
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-28 11:37:41 -0300

    exerc 57

--- CursoemVideo/aula14/ex057.py
+++ CursoemVideo/aula14/ex057.py
@@ -3,3 +3,9 @@ Faça um programa que leia o sexo de uma pessoa,
 mas só aceite valores 'M' ou 'F'. Caso esteja 
 errado, peça a digitação novamente até ter um valor correto.
 """
+nome = input("Digite o nome da pessoa: ").upper().strip()
+genero = str(input("Digite o gênero da pessoa: [F/M]")).upper().strip()
+
+while genero not in 'MmFf':
+    genero = str(input("Gênero inválido. Digite novamente, por favor!\nDigite o sexo da pessoa: [F/M]")).upper().strip()
+print("Gênero {} registrado com sucesso!".format(genero))
