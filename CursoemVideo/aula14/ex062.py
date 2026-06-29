d9410b9e7bfda9ce0be4145c592900a27b3bc743
Nathália Moura <moura.pires.nathalia@gmail.com>
2026-06-29 10:36:35 -0300

    att

--- CursoemVideo/aula14/ex062.py
+++ CursoemVideo/aula14/ex062.py
@@ -2,3 +2,16 @@
 Melhore o Desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. 
 O programa encerra quando ele disser que quer mostrar 0 termos. 
 """
+print("Brincando com Progressao Aritmética")
+n = int(input("Digite o primeiro termo da P.A.: "))
+r = int(input("Digite a razao dessa P.A.: "))
+c = 10
+print("Primeiro termo: {}\nRazão: {}\nP.A: ".format(n, r), end = "")
+i = 1
+while i != 0: 
+    while c != 0: 
+        print("{}".format(n), end = "")
+        print(", " if c > 1 else ".", end = "")
+        n += r
+        c -= 1
+    i = int(input("\nVocê desejar mostrar mais alguns termos? [1/0]"))
